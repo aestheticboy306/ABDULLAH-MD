ceaa41193e9a804b420549225b7b2734c1568117
aestheticboy306 <aestheticboy306@gmail.com>
2025-01-29 17:10:12 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "enter your session",
+    CAPTION: process.env.CAPTION || "POWERED BY ABDULLAH-XER",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "false",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://cdn.ironman.my.id/q/jiYxm.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M ABDULLAH-MD WHATSAPP BOT 😊♻️",
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    ANTI_BAD: process.env.ANTI_BAD || "true",
+    PREFIX: process.env.PREFIX || ".",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+    FAKE_TYPING: process.env.FAKE_TYPING || "false",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "true",
+    READ_CMD: process.env.READ_CMD || "true",
+    BOT_NAME: process.env.BOT_NAME || "𝐀𝐁𝐃𝐔𝐋𝐋𝐀𝐇-𝐌𝐃ャ",
+    STATUS_REPLY: process.env.STATUS_REPLY || "*➺NYC STATUS BRUH...♡︎*",
+    STATUS_REACT: process.env.STATUS_REACT || "true",
+    ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", // change it to 'log' if you want to resend deleted message in ib chat 
+    OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
+};
